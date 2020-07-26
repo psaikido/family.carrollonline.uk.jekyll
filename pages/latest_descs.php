16e2d6a4e643fa9a7d2aa7934703ce1664f665de
Hugh Carroll <hughie@carrollonline.co.uk>
2020-07-26 15:13:37 +0100

    More tidying.

--- pages/latest_descs.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include ('../inc/header.inc');
-include ("../inc/latest_descs.php");
-include ('../inc/footer.inc'); 
-?>
--- pages/latest_pix.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include ('../inc/header.inc');
-include ("../inc/latest_pix.php");
-include ('../inc/footer.inc'); 
-?>
