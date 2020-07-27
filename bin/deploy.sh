9b74dec963070f6a77c20ea1f7080cbbaa0c2405
Hugh Carroll <hughie@carrollonline.co.uk>
2020-07-27 18:09:36 +0100

    Directory moved after a go live.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,4 @@ bundle exec jekyll b
 git push origin master
 
 config="carrollonline_uk@gp-uk.net:~/family.carrollonline.uk/"
-rsync -av --del /home/hughie/sites/carrollonline.uk/family.carrollonline.uk/dev/_site/ $config
+rsync -av --del /home/hughie/sites/carrollonline.uk/family.carrollonline.uk/_site/ $config
